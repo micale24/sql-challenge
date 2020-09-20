63231d8ff7a3945f75ff8c43c244f691b4d81505
micale24 <kaihall.usaf@gmail.com>
2020-09-20 19:55:05 -0400

    finshed database/on to data analyis

--- EmployeeSQL.sql
+++ EmployeeSQL.sql
@@ -3,47 +3,60 @@
 --Deleting table query  
 DROP TABLE departments;
 
---Department Table----
-CREATE TABLE departments(
-	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL
-);
-SELECT * FROM departments;
---------------------------------------
 --Department and Employees
 DROP TABLE dept_emp;
 CREATE TABLE dept_emp(
+	id SERIAL,
 	emp_no Int NOT NULL,
-	dept_no VARCHAR NOT NULL
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (id)
 );
 SELECT * FROM dept_emp;
+--------------------------------------
+--Department Table----
+CREATE TABLE departments(
+	id SERIAL,
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (dept_emp_id) REFERENCES dept_emp(id)
+);
+SELECT * FROM departments;
 --------------------------------------
 --Department Managers
 DROP TABLE dept_manager;
 CREATE TABLE dept_manager(
+	id SERIAl,
 	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL
+	emp_no INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (dept_emp_id) REFERENCES dept_emp(id)
 );
 SELECT * FROM dept_manager;
 --------------------------------------
 --Employees
 DROP TABLE employees;
 CREATE TABLE employees(
+	id SERIAL,
 	emp_no INT NOT NULL,
 	emp_title VARCHAR NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	sex VARCHAR NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (id)
 );
 SELECT * FROM employees;
 --------------------------------------
 --Salaries
 DROP TABLE salaries;
 CREATE TABLE salaries(
+	id SERIAL,
 	emp_no Int,
-	salary Int
+	salary Int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (dept_emp_id) REFERENCES dept_emp(id)
 );
 SELECT * FROM salaries;
 --------------------------------------
@@ -51,7 +64,9 @@ SELECT * FROM salaries;
 DROP TABLE titles;
 CREATE TABLE titles(
 	title_id VARCHAR,
-	title VARCHAR
+	title VARCHAR,
+	PRIMARY KEY (id),
+	FOREIGN KEY (employees_id) REFERENCES employees(id)
 );
 SELECT * FROM titles;
 
