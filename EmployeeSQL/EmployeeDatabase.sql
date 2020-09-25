e45e864126210a0949cebd6b0f43de09be8fc09b
micale24 <kaihall.usaf@gmail.com>
2020-09-25 03:02:40 -0400

    keep moving toward the bounus

--- EmployeeSQL/EmployeeDatabase.sql
+++ EmployeeSQL/EmployeeDatabase.sql
@@ -1,6 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
 --- Database Creation---
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
@@ -73,44 +70,6 @@ REFERENCES "salaries" ("emp_no");
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
 REFERENCES "title" ("title_id");
 
----Data Analysis----
-
---1) List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT salaries.emp_no, first_name, last_name, sex, salary
-FROM salaries
-INNER JOIN employees
-ON salaries.emp_no = employees.emp_no;
-
---2) List first name, last name, and hire date for employees who were hired in 1986.
-SELECT hire_date, first_name, last_name
-FROM employees
-WHERE hire_date 
-BETWEEN '1986-01-01' AND '1986-12-31';
-
---3) List the manager of each department with the following information: 
---department number, department name, the manager's employee number, last name, first name.
-SELECT employees.first_name, employees.last_name, dept_manager.emp_no, departments.dept_name, dept_manager.dept_no
-FROM employees
-INNER JOIN dept_manager 
-ON employees.emp_no=dept_manager.emp_no
-INNER JOIN departments
-ON departments.dept_no=dept_manager.dept_no;
-
-
---4)List the department of each employee with the following information: 
---employee number, last name, first name, and department name.
-
---5)List first name, last name, and sex for employees whose 
---first name is "Hercules" and last names begin with "B."
-
---6)List all employees in the Sales department, 
---including their employee number, last name, first name, and department name.
-
---7)List all employees in the Sales and Development departments, 
---including their employee number, last name, first name, and department name.
-
---8)In descending order, list the frequency count of employee last names, 
---i.e., how many employees share each last name.
 
 
 
